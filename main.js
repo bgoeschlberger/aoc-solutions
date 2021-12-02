84646e25a7b1c27ae0bb874bf27857f3aa9ffc75
Bernhard Göschlberger <bernhard.goeschlberger@researchstudio.at>
2021-12-02 19:05:38 +0100

    adding node backend

---
+++ main.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
+
+app.use(express.static('dist'))
+
+app.listen(port, () => {
+  console.log(`Started server listening at port ${port}`)
+})
